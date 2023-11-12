a4d26eed0435a872dca5864deb7650db1881df7b
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-11-12 21:39:02 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">進撃の巨人 愛好者</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <router-link to="/" class="nav-link">首頁</router-link>
+          <a class="nav-link" href="#">心得</a>
+          <div class="btn-group">
+            <button type="button" class="btn btn-outline-0 btn-transparent dropdown-toggle" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              關於
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><router-link to="/about/AttackOnTitan" class="nav-link">進擊的巨人</router-link></li>
+            </ul>
+          </div>
+          
+          <div class="btn-group">
+            <button type="button" class="btn btn-outline-0 btn-transparent dropdown-toggle" data-bs-toggle="dropdown"
+              data-bs-display="static" aria-expanded="false">
+              帳號
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">登入</a></li>
+              <li><a class="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+
+  <router-view></router-view>
 </template>
 
 <style lang="scss">
@@ -13,6 +45,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  background-color: rgba(207, 207, 207, 0.529);
 }
 
 nav {
---
+++ src/views/giant/AttackOnTitan.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h1>進擊的巨人</h1>
+            <div class="col-4">
+                <img src="https://i.pinimg.com/originals/ae/da/9e/aeda9e87a2608322ee07c1ba245558ed.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-8">
+                <p>
+                    巨人能力：
+                    追求自由的思想
+                    能窺探歷代進巨持有者的記憶
+                    甚至能透過這個能力看到過去或未來
+                    也能夠跟同樣進巨持有者的人進行溝通
+                    並且透過喝下藥劑獲得硬質化能力
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
